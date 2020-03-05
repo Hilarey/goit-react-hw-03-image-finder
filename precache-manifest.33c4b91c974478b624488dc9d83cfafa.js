c217f02dc34b1da8906c66a9dd3569514c7f73e7
Hilarey <piatnytskiy.y.m@gmail.com>
2020-03-05 17:18:04 +0200

    Updates

--- precache-manifest.33c4b91c974478b624488dc9d83cfafa.js
+++ precache-manifest.33c4b91c974478b624488dc9d83cfafa.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6c4f57607226b0318f3b7aa8f70d01f4",
+    "revision": "b84f8d56d29d75cee1d29d1f0094ba99",
     "url": "/goit-react-hw-03-image-finder/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-03-image-finder/static/css/2.f5298653.chunk.css"
   },
   {
-    "revision": "d74e7c6e9739b6cd2659",
+    "revision": "b75f4bdfb240c806afa3",
     "url": "/goit-react-hw-03-image-finder/static/css/main.fdf33732.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-03-image-finder/static/js/2.4faf2976.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d74e7c6e9739b6cd2659",
-    "url": "/goit-react-hw-03-image-finder/static/js/main.cbd236ba.chunk.js"
+    "revision": "b75f4bdfb240c806afa3",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.fce730d9.chunk.js"
   },
   {
     "revision": "9ed00262ab067fcfa080",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-03-image-finder/precache-manifest.b10435cad891da8dc6a9d0977f444b26.js"
+  "/goit-react-hw-03-image-finder/precache-manifest.33c4b91c974478b624488dc9d83cfafa.js"
 );
 
 self.addEventListener('message', (event) => {
